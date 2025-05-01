102b949ecb5ff6abd976ab96f3a22e818a0cf851
vaultboy <thevaultboy11@gmail.com>
2025-05-01 11:08:06 +0200

    adding the clean commit as mentioned

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,11 @@
 // pages/_app.tsx
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
-
-// ← add your Measurement ID here (same as in _document.tsx) the new changes
-const GA4_ID = 'G-HNEK7JXBHE';
-
-/** fires a page_view to gtag */
-const pageview = (url: string) => {
-  if (typeof window !== 'undefined' && (window as any).gtag) {
-    (window as any).gtag('event', 'page_view', {
-      page_path: url,
-      send_to: GA4_ID
-    });
-  }
-};
+import { AuthProvider } from '../context/AuthContext';   // keep your old file
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import Layout from '@/components/Layout';
+import theme from "../theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // initial load
-    pageview(router.asPath);
-
-    // on client-side nav
-    const handleRouteChange = (url: string) => pageview(url);
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline/>
@@ -39,6 +14,6 @@ export default function App({ Component, pageProps }: AppProps) {
          <Component {...pageProps} />
         </Layout>
       </AuthProvider>
-    </ThemeProvider>
+      </ThemeProvider>
   );
 }
